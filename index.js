05f01e5e6da76a74393373b1f7cc2dc0eb2f0cdb
shen <ratonshenelaeloreece@gmail.com>
2025-11-05 17:38:23 +0800

    changes on toggler button and menu - icon, icon style, script for collapsing menu when clicking outside, addition of bootstrap icons

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const navbar = document.getElementById("main-navbar");
 const navbarHeight = navbar.offsetHeight;
-console.log(`Navbar Height: ${navbarHeight}px`);
+// console.log(`Navbar Height: ${navbarHeight}px`);
 const sections = document.querySelectorAll("section");
 
 sections.forEach(section => {
@@ -10,3 +10,22 @@ sections.forEach(section => {
 window.onload = function() {
   this.document.getElementById("contact-form").reset();
 };
+
+document.addEventListener('DOMContentLoaded', function(){
+
+  document.addEventListener('click', function(event){
+    // toggler button and menu
+    const togglerButton = document.querySelector(".navbar-toggler");
+    const collapsibleMenu = document.querySelector(".navbar-collapse");
+    const isMenuClosed = togglerButton.classList.contains('collapsed');
+    const trigger = event.target
+
+    if (!isMenuClosed) {
+      if ((!togglerButton.contains(trigger)) && (!collapsibleMenu.contains(trigger))) {
+        togglerButton.classList.add("collapsed");
+        collapsibleMenu.classList.remove("show");
+        
+      }
+    }
+  });
+});
--- scripts/home.js
+++ scripts/home.js
@@ -2,25 +2,11 @@
 const authorName = document.querySelector(".landing-name");
 const nameHeight = authorName.offsetHeight;
 const mediaQueryLaptop = "(min-width: 1024px)";
-// const mql = window.matchMedia(mediaQueryLaptop);
-
-// mql.addEventListener("resize", (event) => {
-//   if (event.matches){
-//     const descSection = document.querySelector(".landing-desc");
-//     descSection.style.paddingTop = `${nameHeight}px`;
-//   }
-// });
-
-// if (window.matchMedia(mediaQueryLaptop)) {
-//   const descSection = document.querySelector(".landing-desc");
-//   descSection.style.paddingTop = `${nameHeight - 10}px`;
-// }
-
 
 function applyStyleForLaptop() {
   const currentWidth = window.innerWidth;
   const landingDesc = document.querySelector(".landing-desc");
-  console.log(`Current Width: ${currentWidth}`)
+  // console.log(`Current Width: ${currentWidth}`)
   if (currentWidth >= 1023) {
     landingDesc.style.paddingTop = `${nameHeight -10}px`;
   }
